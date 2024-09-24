5d529cefcaf9ccf53c7772f230b4395caee8431c
NGUYEN HAI HOAI DUC <fizzducnguyen@gmail.com>
2024-09-24 23:23:46 +0700

    step 7

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,4 +1,81 @@
+import React from "react";
+import { Button, Form, Input, notification } from "antd";
+import { loginApi } from "../util/api";
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
-  return <>login page</>;
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    const { email, password } = values;
+
+    const res = await loginApi(email, password);
+
+    if (res && res.EC == 0) {
+      localStorage.setItem("access_token", res.access_token);
+      notification.success({
+        message: "LOGIN USER",
+        description: "Success",
+      });
+      navigate("/");
+    } else {
+      console.log("WRONGGGGGGGGGGGGGGGGG");
+      notification.error({
+        message: "LOGIN USER",
+        description: res?.EM ?? "error",
+      });
+    }
+    console.log(">>>>>>>>Success:", values);
+  };
+
+  return (
+    <div style={{ margin: 50 }}>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        onFinish={onFinish}
+        autoComplete="off"
+        layout="vertical"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Email!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 export default LoginPage;
